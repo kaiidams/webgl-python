eb2e5a32f5dbfd45edaaeaf7c573a77a9727ec22
Katsuya Iida <katsuya.iida@gmail.com>
2023-12-31 02:58:59 +0000

    update js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const PROTOCOL_VERSION = "pywebgl-0.1";
+const ERROR_INTERNAL = -32603;
+
 class TransportWebSocket {
-    constructor(addr) {
-        this.addr = addr;
+    constructor(uri) {
+        this.uri = uri;
         this.ws = null;
         this.server = null;
         this.serverProxies = {};
@@ -10,28 +13,30 @@ class TransportWebSocket {
 
     start(server) {
         this.server = server;
-        this.ws = new WebSocket(this.addr);
+        this.ws = new WebSocket(this.uri);
 
-        this.ws.onopen = async (event) => {
-            this.send({
-                destination: null,
-                method: "__register__",
-                params: [server.name]
-            });
+        this.ws.onopen = (event) => {
+            if (this.server != null) {
+                this.send({
+                    destination: null,
+                    method: "__register__",
+                    params: [server.name]
+                });
+            }
         };
         
         this.ws.onmessage = (event) => {
             try {
                 const data = JSON.parse(event.data);
                 console.log('<--', data);
-                if (data.result !== undefined) {
+                if (data.result !== undefined || data.error !== undefined) {
                     const proxy = this.serverProxies[data.source];
                     proxy.onReceive(data);
                 } else {
                     this.server.onReceive(data);
                 }
-            } catch (ex) {
-                console.error(ex);
+            } catch (e) {
+                console.error(e);
             }
         };
 
@@ -46,7 +51,7 @@ class TransportWebSocket {
 
     send(data) {
         console.log('-->', data);
-        data.jsonrpc = "pywebgl-0.1";
+        data.jsonrpc = PROTOCOL_VERSION;
         this.ws.send(JSON.stringify(data));
     }
 }
@@ -58,21 +63,28 @@ class Server {
         this.nextObjectId = 0;
         this.liveObjects = {};
         this.global = { window: window };
-        this.methods = {
-            "__getter__": (params) => {
-                let target = params[0] == null ? this.global : params[0];
-                let name = params[1];
+        this.methods = {}
+    }
+
+    registerDefaultMethods() {
+        this.registerMethod(
+            "__getter__",
+            (target, name) => {
+                if (target == null) target = this.global;
                 return target[name];
-            },
-            "__new__": (params) => {
-                const name = params.shift();
-                const constructor = window[name].bind(null, ...params);
-                return new constructor();
-            },
-            "__inspect__": (params) => {
+            });
+        this.registerMethod(
+            "__new__",
+            (name, ...args) => {
+                const f = window[name].bind(null, ...args);
+                return new f();
+            });
+        this.registerMethod(
+            "__inspect__",
+            (name) => {
                 let methods = [];
                 let properties = [];
-                const constructor = window[params[0]];
+                const constructor = window[name];
                 if (constructor === undefined) {
                     return null;
                 }
@@ -93,8 +105,11 @@ class Server {
                     }
                 }
                 return {parent: parent, methods: methods, properties: properties};
-            }
-        };
+            });
+    }
+
+    registerMethod(name, method) {
+        this.methods[name] = method;
     }
 
     serve() {
@@ -106,7 +121,7 @@ class Server {
             const params = this.unmarshalParams(data.params);
             let result;
             if (this.methods.hasOwnProperty(data.method)) {
-                result = this.methods[data.method](params);
+                result = this.methods[data.method](...params);
             } else {
                 const target = params.shift();
                 result = target[data.method].apply(target, params);
@@ -116,14 +131,14 @@ class Server {
                 destination: data.source,
                 result: this.marshalResult(result)
             });
-        } catch (ex) {
-            console.error(ex);
+        } catch (e) {
+            console.error(e);
             this.transport.send({
                 id: data.id,
                 destination: data.source,
                 error: {
-                    code: -32603,
-                    message: ex.message
+                    code: ERROR_INTERNAL,
+                    message: e.message
                 }
             });
         }
@@ -133,35 +148,40 @@ class Server {
         return params.map((value) => {
             if (value instanceof Array) {
                 return value;
-            } else if (value !== null && typeof(value) === "object") {
-                return this.liveObjects[value.id];
-            } else {
+            }
+            if (value instanceof Object) {
+                if (value.__jsonclass__ !== undefined) {
+                    const objectId = value.__jsonclass__[1];
+                    return this.liveObjects[objectId];
+                }
                 return value;
             }
+            // number, string, null
+            return value;
         });
     }
 
-    marshalResult(result) {
-        if (result === undefined || result == null) {
+    marshalResult(value) {
+        if (value === undefined || value == null) {
             return null;
         }
-        if (typeof(result) !== "object") {
-            return result;
+        if (typeof(value) !== "object") {
+            return value;
         }
-        if (Object.getPrototypeOf(result) === Object.prototype) {
-            return result;
+        if (Object.getPrototypeOf(value) === Object.prototype) {
+            return value;
         }
-        if (Object.getPrototypeOf(result) === Array.prototype) {
-            return result;
+        if (Object.getPrototypeOf(value) === Array.prototype) {
+            return value;
         }
-        if (result._objectId === undefined) {
+        if (value._objectId === undefined) {
             const objectId = this.nextObjectId++;
-            this.liveObjects[objectId] = result;
-            result._objectId = objectId;
+            this.liveObjects[objectId] = value;
+            value._objectId = objectId;
         }
-        const constructor = Object.getPrototypeOf(result).constructor.name;
+        const constructor = Object.getPrototypeOf(value).constructor.name;
         return {
-            __jsonclass__: [constructor, result._objectId]
+            __jsonclass__: [constructor, value._objectId]
         };
     }
 }
@@ -171,7 +191,6 @@ class ServerProxy {
         this.transport = transport;
         this.name = name;
         this.nextRequestId = 0;
-        this.liveObjects = {};
         this.pendingRequests = {};
         this.transport.serverProxies[this.name] = this;
     }
@@ -190,22 +209,33 @@ class ServerProxy {
     }
 
     onReceive(data) {
-        let resolve = this.pendingRequests[data.id];
+        const resolve = this.pendingRequests[data.id];
         delete this.pendingRequests[data.id];
         resolve(this.unmarshalResult(data.result));
     }
 
-    unmarshalResult(result) {
-        if (result !== null && typeof(result) === 'object') {
-            return new ObjectProxy(this, result.id);
+    unmarshalResult(value) {
+        if (value instanceof Array) {
+            return value;
         }
-        return result;
+        if (value instanceof Object) {
+            if (value.__jsonclass__ !== undefined) {
+                const constructor = value.__jsonclass__[0];
+                const objectId = value.__jsonclass__[1];
+                return new ObjectProxy(this, constructor, objectId);
+            }
+            return value;
+        }
+        // number, string, null
+        return value;
     }
 
     marshalParams(params) {
         return params.map((value) => {
             if (value instanceof ObjectProxy) {
-                return { id: value.objectId };
+                return {
+                    __jsonclass__: [value.constructor, value.objectId]
+                };
             }
             return value;
         });
@@ -213,44 +243,14 @@ class ServerProxy {
 }
 
 class ObjectProxy {
-    constructor(proxy, objectId) {
+    constructor(proxy, constructor, objectId) {
         this.proxy = proxy;
+        this.constructor = constructor;
         this.objectId = objectId;
     }
 
     async invoke(method, ...args) {
-        args.unshift(this.objectId === null ? null : this);
+        args.unshift(this.objectId == null ? null : this);
         return await this.proxy.request(method, args);
     }
 }
-
-window.addEventListener("load", async () => {
-    console.log(window.location.search);
-    const name = window.location.search.replace('?name=', '');
-    const remote = name == 'server_a' ? 'server_b' : 'server_a';
-    const addr = `ws://${window.location.host}/ws`;
-    const transport = new TransportWebSocket(addr);
-    const server = new Server(name, transport);
-    const proxy1 = new ServerProxy(remote, transport);
-    const proxy2 = new ServerProxy('server_c', transport);
-    server.serve();
-
-    const test1Elem = document.getElementById("test1");
-    test1Elem.addEventListener("click", async (event) => {
-        event.preventDefault();
-        let g = new ObjectProxy(proxy1, null);
-        let win = await g.invoke("__getter__", "window");
-        let doc = await win.invoke("__getter__", "document");
-        let body = await doc.invoke("__getter__", "body");
-        //let res = await win.invokeMethod("alert", ["Hi!"]);
-        let x = await body.invoke("__getter__", "innerHTML");
-        console.log(x);
-    });
-
-    const test2Elem = document.getElementById("test2");
-    test2Elem.addEventListener("click", async (event) => {
-        event.preventDefault();
-        let g = new ObjectProxy(proxy2, null);
-        await g.invoke("start");
-    });
-});
